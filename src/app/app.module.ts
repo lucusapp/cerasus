1c3a2617d5cd2c7dfb600fa2e450ac0ca166c2bd
gavialus <lucusapp@gmail.com>
2017-11-01 14:47:50 +0100

    Cargados header/body/footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProfolioComponent } from './components/profolio/profolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProfolioComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/profolio/profolio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profolio',
+  templateUrl: './profolio.component.html',
+  styles: []
+})
+export class ProfolioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
